d64d46b49a03a38cfa610dfd4b3e6b9cdc2651a5
AlexPankratov1992 <139518798+AlexPankratov1992@users.noreply.github.com>
2023-09-05 08:09:38 -0500

    try four different materials. basic, normal, phong, standard materials. and control material values via leva control

--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,27 +1,44 @@
 import {useEffect, useRef, useState} from "react";
 import {useFrame} from "@react-three/fiber";
+import {useControls} from "leva";
+import * as THREE from "three";
 
 export const Polyhedron = ({color, polyhedron, ...props}) => {
     const ref = useRef()
-    const [count, setCount] = useState(0);
-
-    //console.log(polyhedron)
 
     useFrame((_, delta)=>{
-        ref.current.rotation.x += delta;
-        ref.current.rotation.y -= 0.5 * delta;
+        ref.current.rotation.x += 0.2 * delta
+        ref.current.rotation.y += 0.05 * delta
     })
 
-    useEffect(()=>console.log(ref.current.geometry.uuid))
+    useControls(props.name, {
+        wireframe: {
+            value: false,
+            onChange: (v) => {
+                ref.current.material.wireframe = v
+            },
+        },
+        flatShading: {
+            value: true,
+            onChange: (v) => {
+                ref.current.material.flatShading = v
+                ref.current.material.needsUpdate = true
+            },
+        },
+        color: {
+            value: 'lime',
+            onChange: (v) => {
+                ref.current.material.color = new THREE.Color(v)
+            },
+        },
+    })
 
     return (
         <mesh
             {...props}
             ref={ref}
-            onPointerDown={()=>setCount((count+1)%3)}
-            geometry={polyhedron[count]}
         >
-            <meshBasicMaterial color={color} wireframe />
+            <icosahedronGeometry args={[1, 1]} />
         </mesh>
     )
 }
