e24fcd4b09cfb97eec327e845d3b352425e6bb6a
AlexPankratov1992 <139518798+AlexPankratov1992@users.noreply.github.com>
2023-09-05 00:25:32 -0500

    useLayoutEffect for overcoming drawback of asynchronous behaviour of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import {Canvas} from "@react-three/fiber";
 
 export default function App() {
     return (
-        <Canvas camera={{position:[0, 0, 2]}}>
+        <Canvas camera={{position:[0, 0, 2]}} frameloop="demand">
             <Box position={[-0.65, 0, 0]} name="A"/>
-            <Box position={[+0.65, 0, 0]} name="A"/>
+            <Box position={[+0.65, 0, 0]} name="B"/>
         </Canvas>
     )
 }
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,10 +1,19 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useLayoutEffect, useRef} from "react";
 
 export const Box = (props) => {
     const ref = useRef();
 
     useEffect(()=>{
         console.log("ref: ", ref);
+        if (ref.current.name === 'B') {
+            ref.current.position.y = 1
+        }
+    })
+
+    useLayoutEffect(()=>{
+        if (ref.current.name === 'A') {
+            ref.current.position.y = 1
+        }
     })
 
     return (
