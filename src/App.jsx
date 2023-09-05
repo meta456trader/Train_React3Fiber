6a69df0c61c82de7f57a3e15d222561cde9b5136
AlexPankratov1992 <139518798+AlexPankratov1992@users.noreply.github.com>
2023-09-05 01:14:41 -0500

    mouse click/release, mouse hover/out events

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Canvas} from "@react-three/fiber";
 
 export default function App() {
     return (
-        <Canvas camera={{position:[0, 0, 2]}} frameloop="demand">
+        <Canvas camera={{position:[0, 0, 2]}} frameloop="always">
             <Box position={[-0.65, 0, 0]} name="A"/>
             <Box position={[+0.65, 0, 0]} name="B"/>
         </Canvas>
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useLayoutEffect, useRef} from "react";
+import {useFrame} from "@react-three/fiber";
 
 export const Box = (props) => {
     const ref = useRef();
@@ -16,8 +17,18 @@ export const Box = (props) => {
         }
     })
 
+    useFrame((_, delta)=>{
+        ref.current.rotation.x += 1 * delta;
+        ref.current.rotation.y -= 0.5 * delta;
+    })
+
     return (
-        <mesh {...props} ref={ref}>
+        <mesh {...props} ref={ref}
+              onPointerDown={(e) => console.log('pointer down ' + e.object.name)}
+              onPointerUp={(e) => console.log('pointer up ' + e.object.name)}
+              onPointerOver={(e) => console.log('pointer over ' + e.object.name)}
+              onPointerOut={(e) => console.log('pointer out ' + e.object.name)}
+              onUpdate={(self) => console.log(self)}>
             <boxGeometry/>
             <meshBasicMaterial color={0x00ff00} wireframe/>
         </mesh>
